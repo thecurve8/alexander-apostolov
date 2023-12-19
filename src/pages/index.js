eeeaa15dd108ab0328767f4bafff1dafdba913c6
Aleander Apostolov <alexander.apostolov@epfl.ch>
2023-12-19 17:41:37 +0100

    Reverting

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,32 +17,19 @@ const StyledMainContainer = styled.main`
   counter-reset: section;
 `;
 
-const IndexPage = ({ location }) => {
-    const [darkMode, setDarkMode] = React.useState(false)
-    const toggleDarkMode = () => {
-        setDarkMode(prev => !prev)
-    }
-    return (
-    <div className={darkMode ? 'app dark' : 'app'}>
-        <Layout location={location}>
-            <StyledMainContainer className="fillHeight">
-
-                <Hero/>
-
-                    <button onClick={toggleDarkMode}>
-                        toggle dark mode
-                    </button>
-
-                <About/>
-                <Education/>
-                <Featured/>
-                <Projects/>
-                <Jobs/>
-                <Contact/>
-            </StyledMainContainer>
-        </Layout>
-    </div>
-)};
+const IndexPage = ({ location }) => (
+    <Layout location={location}>
+      <StyledMainContainer className="fillHeight">
+        <Hero />
+        <About />
+        <Education />
+        <Featured />
+        <Projects />
+        <Jobs />
+        <Contact />
+      </StyledMainContainer>
+    </Layout>
+);
 
 
 IndexPage.propTypes = {
